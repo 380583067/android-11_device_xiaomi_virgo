e648eb347232cbc7c5bb41b4c45dca034a706891
apple <105837597+380583067@users.noreply.github.com>
2023-05-02 01:16:52 +0000

    Update and rename mokee_virgo.mk to dot_virgo.mk

--- dot_virgo.mk
+++ dot_virgo.mk
@@ -19,10 +19,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_k.mk)
 # Inherit from virgo device
 $(call inherit-product, device/xiaomi/virgo/virgo.mk)
 
-# Inherit some common Mokee stuff.
-$(call inherit-product, vendor/mokee/config/common_full_phone.mk)
+# Inherit some common dot stuff.
+$(call inherit-product, vendor/dot/config/common.mk)
 
-PRODUCT_NAME := mokee_virgo
+PRODUCT_NAME := dot_virgo
 PRODUCT_DEVICE := virgo
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
